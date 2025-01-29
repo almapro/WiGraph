9492d7fa6c19d37fced4a09e0fed45139d87b670
ALMA PRO <alma.pro.leader@gmail.com>
2025-01-29 07:37:37 +0200

    feat: creates app store using zustand

--- src/nodes/types.ts
+++ src/nodes/types.ts
@@ -1,6 +1,23 @@
-import type { Node, BuiltInNode } from "@xyflow/react";
+import type {
+  Node,
+  BuiltInNode,
+  OnNodesChange,
+  OnEdgesChange,
+  Edge,
+  OnConnect,
+} from "@xyflow/react";
 import { Wifi } from "../nodes.types";
 
 export type PositionLoggerNode = Node<{ label: string }, "position-logger">;
 export type WifiNode = Node<Wifi, "wifi">;
 export type AppNode = BuiltInNode | PositionLoggerNode | WifiNode;
+
+export type AppState = {
+  nodes: AppNode[];
+  edges: Edge[];
+  onNodesChange: OnNodesChange<AppNode>;
+  onEdgesChange: OnEdgesChange;
+  onConnect: OnConnect;
+  setNodes: (nodes: AppNode[]) => void;
+  setEdges: (edges: Edge[]) => void;
+};
